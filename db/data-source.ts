dc5ef7c5c8d6058e21d175db5c4f2168c72e3b61
Mohammadreza Maddahi <mrmahan532@gmail.com>
2025-02-21 18:47:30 +0330

    change data souce

--- db/data-source.ts
+++ db/data-source.ts
@@ -5,11 +5,11 @@ import { DataSource, DataSourceOptions } from 'typeorm';
 
 export const dataSourceOption: DataSourceOptions = {
     type: 'mysql',
-    host: process.env.DATABASE_HOST,
-    port: Number(process.env.DATABASE_PORT),
-    username: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASS,
-    database: process.env.DATABASE_NAME,
+    host: "book-db",
+    port: 3306,
+    username: "root",
+    password: "WV85ZzLWVy6fJRkddQRdNCzh",
+    database: "elegant_liskov",
     entities: ['dist/**/*.entity{.js,.ts}'],
     migrations: [__dirname + '/migrations/*.js'],
     logging: false,
