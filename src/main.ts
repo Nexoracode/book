20ad613be260132b80994a9fb2b4927ce29d0248
Mohammadreza Maddahi <mrmahan532@gmail.com>
2025-02-22 23:57:56 +0330

    main chnage enbale cors

--- src/main.ts
+++ src/main.ts
@@ -2,19 +2,18 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { AllExceptionsFilter } from './common/filters/exception';
-import * as dotenv from 'dotenv';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe);
-  // app.useGlobalFilters(new AllExceptionsFilter()),
-  app.enableCors({
-    // origin: ['https://bookshops.liara.run'],
-    origin: '*',
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    credentials: true,
-  })
+  app.useGlobalFilters(new AllExceptionsFilter()),
+    app.enableCors({
+      origin: ['https://bookshops.liara.run'],
+      // origin: '*',
+      methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+      credentials: true,
+    })
 
-  await app.listen(process.env.PORT || 3000, '0.0.0.0');
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
