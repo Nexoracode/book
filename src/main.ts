1a5df59652dc2b0b8917f4cd57546386c2a6a4d1
Mohammadreza Maddahi <mrmahan532@gmail.com>
2025-02-23 00:01:12 +0330

    main corse

--- src/main.ts
+++ src/main.ts
@@ -8,8 +8,8 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe);
   app.useGlobalFilters(new AllExceptionsFilter()),
     app.enableCors({
-      origin: ['https://bookshops.liara.run'],
-      // origin: '*',
+      // origin: ['https://bookshops.liara.run'],
+      origin: '*',
       methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
       credentials: true,
     })
