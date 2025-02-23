7acef78aa674f45df57d93ee14aafb42fcc69927
Mohammadreza Maddahi <mrmahan532@gmail.com>
2025-02-23 11:12:35 +0330

    main enbale cors with liara book and loacal 3002

--- src/main.ts
+++ src/main.ts
@@ -6,12 +6,13 @@ import { AllExceptionsFilter } from './common/filters/exception';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe);
-  app.useGlobalFilters(new AllExceptionsFilter()),
-    app.enableCors({
-      origin: ['https://bookshops.liara.run'],
-      methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-      credentials: true,
-    })
+  // app.useGlobalFilters(new AllExceptionsFilter()),
+  app.enableCors({
+    origin: ['https://bookshops.liara.run', 'http://localhost:3002'],
+    // origin: '*',
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
+  })
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
